c0cc737e9b5129ae8f3b4f83e62ffade07087bdf
Daniel <daniell-henrique@hotmail.com>
2020-03-27 14:13:47 -0300

    Adding dummy data

--- marioplan-network/src/store/reducers/projectReducer.js
+++ marioplan-network/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'help me find princess peach', content: 'she is in browser\'s castle'},
+        {id: '2', title: 'collect all the stars', content: 'some of them are hidden'},
+        {id: '3', title: 'egg hunt with yoshi', content: 'in the fields of Hyrule'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
