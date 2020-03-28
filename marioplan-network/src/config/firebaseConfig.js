bec38adf777b10d329c787c884bf39e61c44b161
Daniel <daniell-henrique@hotmail.com>
2020-03-28 12:36:31 -0300

    Configuring Firestore Data and Collections

---
+++ marioplan-network/src/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+import firebase from './firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCFKTKZ0sZdA8_UrTuQGXPz4qivt7OkCR0",
+    authDomain: "marioplan-network.firebaseapp.com",
+    databaseURL: "https://marioplan-network.firebaseio.com",
+    projectId: "marioplan-network",
+    storageBucket: "marioplan-network.appspot.com",
+    messagingSenderId: "1050025860164",
+    appId: "1:1050025860164:web:8f3e93b40b701a06cfb011",
+    measurementId: "G-9T6VYXZ7F7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({timeStampsInSnapshots: true})
+
+export default firebase
