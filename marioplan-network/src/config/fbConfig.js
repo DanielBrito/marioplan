c0369140b6886873aa644ec853c95c909a7defca
Daniel <daniell-henrique@hotmail.com>
2020-08-09 15:31:24 -0300

    Configuration

--- marioplan-network/src/config/fbConfig.js
+++ marioplan-network/src/config/fbConfig.js
@@ -4,14 +4,14 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 export var firebaseConfig = {
-    apiKey: "AIzaSyCFKTKZ0sZdA8_UrTuQGXPz4qivt7OkCR0",
-    authDomain: "marioplan-network.firebaseapp.com",
-    databaseURL: "https://marioplan-network.firebaseio.com",
-    projectId: "marioplan-network",
-    storageBucket: "marioplan-network.appspot.com",
-    messagingSenderId: "1050025860164",
-    appId: "1:1050025860164:web:8f3e93b40b701a06cfb011",
-    measurementId: "G-9T6VYXZ7F7"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
 };
 
 // Initialize Firebase
